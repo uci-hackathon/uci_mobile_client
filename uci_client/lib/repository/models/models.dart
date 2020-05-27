f2b4b334de22d2cbd054e53d3841e24bba120626
Eugene Velizhenkov <tredasaibot@gmail.com>
2020-05-27 18:33:26 +0200

    update models

--- uci_client/lib/repository/models/models.dart
+++ uci_client/lib/repository/models/models.dart
@@ -1,1 +1,17 @@
 export 'account.dart';
+
+class Proposal {
+  String proposalId;
+  String ballotName;
+  String proposer;
+  double amountRequested;
+  String body;
+}
+
+class Ballot {
+  String ballotName;
+  String category;
+  String publisher;
+  double treasurySymbol;
+  String body;
+}
